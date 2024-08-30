e7471dcfdac8473c7db29e1a576bc93094fee6cb
killerwhale1125 <killerwhale1125@naver.com>
2024-08-30 17:20:34 +0900

    V1 Trace 로깅 추가 - 예외 구분 완료 - 클래스 구분

---
+++ src/main/java/study/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,33 @@
+package study.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import study.advanced.trace.TraceStatus;
+import study.advanced.trace.hellotrace.HelloTraceV1;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(@RequestParam("itemId") String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+        } catch(Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+        return "ok";
+    }
+
+
+}
