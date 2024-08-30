60b3189a08c978d401216c80f4390275b56fa25c
killerwhale1125 <killerwhale1125@naver.com>
2024-08-30 21:00:23 +0900

    Trace 빈 설정 ThreadLocal 변경

---
+++ src/main/java/study/advanced/LogTraceConfig.java
@@ -0,0 +1,14 @@
+package study.advanced;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import study.advanced.trace.logtrace.LogTrace;
+import study.advanced.trace.logtrace.ThreadLocalLogTrace;
+
+@Configuration
+public class LogTraceConfig {
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
+}
